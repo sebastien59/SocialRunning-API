c98640ba8f0c46c77b68b9fb1f69655280ac48c9
Sebastien Merchez <merchez.sebastien@gmail.com>
2017-03-25 11:53:14 +0100

    changement port d'écoute

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.get('/api/restricted', function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log('listening on http://localhost:3000');
+app.listen(3001, function () {
+  console.log('listening on http://localhost:3001');
 });
